686fad1d34d7c6c5f46f2db66c8ecee2ddd2dba1
karim <kemomohammed155@gmail.com>
2025-03-20 16:01:01 +0200

    post&interaction&comment

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./DB/connection.js";
+import {
+  category_router,
+  comment_router,
+  interaction_router,
+  post_router,
+  user_router,
+} from "./src/modules/index.js";
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT;
+
+await connectDB();
+
+app.use(express.json());
+app.use("/user", user_router);
+app.use("/category", category_router);
+app.use("/post", post_router);
+app.use("/interaction", interaction_router);
+app.use("/comment", comment_router);
+
+app.all("/*", (req, res, next) => {
+  return next(new Error("Page not found", { cause: 404 }));
+});
+
+app.use((error, req, res, next) => {
+  const statusCode = error.cause || 500;
+  res.status(statusCode).json({
+    success: false,
+    message: error.message,
+    stack: error.stack,
+  });
+});
+
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
