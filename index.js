1ad72eb4d9781ca4867fdde4fdd09587f503f615
karim <kemomohammed155@gmail.com>
2025-04-15 14:10:02 +0200

    update

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ import {
   sub_category_router,
   user_router,
 } from "./src/modules/index.js";
+import { global_response } from "./src/middleware/index.js";
 const app = express();
 dotenv.config();
 
 const port = process.env.PORT;
 
-await connectDB();
 
 app.use(express.json());
 app.use("/user", user_router);
@@ -23,20 +23,9 @@ app.use("/sub_category", sub_category_router);
 app.use("/post", post_router);
 app.use("/interaction", interaction_router);
 app.use("/comment", comment_router);
+app.use(global_response);
 
-app.all("/*", (req, res, next) => {
-  return next(new Error("Page not found", { cause: 404 }));
-});
 
-app.use((error, req, res, next) => {
-  const statusCode = error.cause || 500;
-  res.status(statusCode).json({
-    success: false,
-    message: error.message,
-    stack: error.stack,
-  });
-});
-
-app.listen(port, () =>
-  console.log(`App listening at http://localhost:${port}`)
-);
+connectDB();
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
