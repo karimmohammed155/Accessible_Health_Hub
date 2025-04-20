e30ef67ca073cb3e4351d400d3c185552b10aee9
karim <kemomohammed155@gmail.com>
2025-04-20 13:43:29 +0200

    last update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"
 import { connectDB } from "./DB/connection.js";
 import {
   category_router,
@@ -10,6 +11,8 @@ import {
   user_router,
 } from "./src/modules/index.js";
 import { global_response } from "./src/middleware/index.js";
+import setup_socket from "./src/utils/socket.js";
+
 const app = express();
 dotenv.config();
 
@@ -17,6 +20,7 @@ const port = process.env.PORT;
 
 await connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use("/user", user_router);
 app.use("/category", category_router);
@@ -24,7 +28,7 @@ app.use("/sub_category", sub_category_router);
 app.use("/post", post_router);
 app.use("/interaction", interaction_router);
 app.use("/comment", comment_router);
-app.use(global_response)
+app.use(global_response);
 
 app.all("/*", (req, res, next) => {
   return next(new Error("Page not found", { cause: 404 }));
@@ -39,6 +43,8 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.listen(port, () =>
+let server = app.listen(port, () =>
   console.log(`App listening at http://localhost:${port}`)
 );
+
+setup_socket(server);
