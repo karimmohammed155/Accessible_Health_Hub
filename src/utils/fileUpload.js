10d8b13e877bc4ac6b1cb7ffd736b7b351c1e5b2
Yara Megawer <108768001+yaramegawer@users.noreply.github.com>
2025-06-03 19:13:01 +0300

    Update fileUpload.js

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -2,6 +2,11 @@
 import multer, { diskStorage } from "multer";
 import path from "path";
 
+const uploadDir = path.join(process.cwd(), 'uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
 export const fileAudioUpload = () => {
   const storage = diskStorage({
     destination: (req, file, cb) => {
