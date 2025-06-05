65b9de7859e471117d21abbbaaaefa38668c305a
Yara Sayed <yara.yarasayed@gmail.com>
2025-06-05 12:20:55 +0300

    resolved package.json errors

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -2,19 +2,7 @@
 import multer, { diskStorage } from "multer";
 import path from "path";
 // middleware/cloudinaryUpload.js
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-import {cloudinary} from './cloudinary.js';
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'audio_uploads',
-    resource_type: 'auto', // Allows audio files
-    format: async (req, file) => file.originalname.split('.').pop(),
-    public_id: (req, file) => `${Date.now()}-${Math.round(Math.random() * 1e9)}`,
-  },
-});
 
 
 export const fileUpload=()=>{
@@ -27,6 +15,3 @@ export const fileUpload=()=>{
     return multer({storage:diskStorage({})});
 }
 
-const parser = multer({ storage });
-
-export  {parser};
